d684b702486d205dc74053f4d99fdf2f456cc7f4
sasinduSilva <sinharaavishka@gmail.com>
2020-11-03 21:00:43 +0530

    initial commit

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/braavoCode/ecommerce003/config/MyDataRestConfig.java
@@ -0,0 +1,69 @@
+package com.braavoCode.ecommerce003.config;
+
+import javax.persistence.metamodel.EntityType;
+
+import com.braavoCode.ecommerce003.Entity.Country;
+import com.braavoCode.ecommerce003.Entity.Product;
+import com.braavoCode.ecommerce003.Entity.ProductCategory;
+import com.braavoCode.ecommerce003.Entity.State;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+   @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager){
+
+        entityManager = theEntityManager;
+
+    }
+
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config){
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST,HttpMethod.DELETE};
+
+        disableHttpMethods(Product.class,config, theUnsupportedActions);
+        disableHttpMethods(ProductCategory.class,config,theUnsupportedActions);
+        disableHttpMethods(Country.class,config,theUnsupportedActions);
+        disableHttpMethods(State.class,config,theUnsupportedActions);
+
+        exposeIds(config);
+
+
+    }
+
+    private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions){
+       config.getExposureConfiguration()
+               .forDomainType(theClass)
+               .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+               .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+    }
+
+
+
+
+
+    private void exposeIds(RepositoryRestConfiguration config){
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+
+        for (EntityType  tempEntityType: entities){
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+    }
+}
---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/braavoCode/ecommerce003/dao/StateRepository.java
@@ -0,0 +1,4 @@
+package com.braavoCode.ecommerce003.dao;
+
+public interface StateRepository {
+}
