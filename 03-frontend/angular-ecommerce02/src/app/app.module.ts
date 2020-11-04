5016c65e87217366c67ee959707460c329a942ae
sasinduSilva <sinharaavishka@gmail.com>
2020-11-04 13:23:42 +0530

    create Product-Category-Menu-Component

--- 03-frontend/angular-ecommerce02/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce02/src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { Routes } from '@angular/router';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+
+
+
+
+const routes: Routes = [
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'},
+];
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductCategoryMenuComponent
   ],
   imports: [
     BrowserModule
